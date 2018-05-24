7e96479f752c7bb59c4acc01ea7c32a28df3f8a8
Isaac Ash-Johnson <isaacashj@gmail.com>
2018-05-24 07:52:47 -0500

    Methods for reading and storing data from serial

---
+++ collecting/collect.py
@@ -0,0 +1,26 @@
+import serial
+from collecting import dev_info
+
+try:
+    arduino = serial.Serial(dev_info.DEVICE_PORT, timeout=dev_info.DEFAULT_TIMEOUT, baudrate=dev_info.DEFAULT_BAUDRATE)
+except:
+    print('Communication error. Check connection to device.')
+
+
+def collect_raw_data(list_length):
+    # Skips first empty line
+    arduino.readline()
+
+    raw_data = []
+    while len(raw_data) < list_length:
+        # info is the first line with values
+        info = str(arduino.readline(), 'utf-8')
+
+        raw_data.append(info)
+        print(info)
+    return raw_data
+
+
+def scrub_data(data_list):
+
+    return data_list
