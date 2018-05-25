6b7f801cf9bd95268d16af278cf52c1ba8a6488f
Isaac Ash-Johnson <isaacashj@gmail.com>
2018-05-25 08:19:49 -0500

    Improved collecting and added documentation

--- collecting/__init__.py
+++ collecting/__init__.py
@@ -1,6 +1,20 @@
 from collecting import collect
+import re
 
-thing = []
-thing = collect.collect_raw_data(3)
-thing = collect.scrub_data(thing)
-print(str(thing))
+# Open in write mode. This will clear the file of it's contents.
+data_file = open('../data.csv', 'w')
+
+# CSV headers.
+data_file.write("time,value\n")
+
+# Fetch the data from the Arduino
+data = collect.collect_raw_data(300)
+print(str(data))
+
+# Get rid of unnecessary formatting; convert to CSV-compatible format.
+data = re.sub('[\[\]\' ]', '', str(data))
+data = re.sub(',', '\n', data)
+data = re.sub(':', ',', data)
+
+# Write data to file
+data_file.write(data)
